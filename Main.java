2ca4ace6860de4f9a3e78334c0b5e6a1609ca617
Surajghogare80 <surajghogare1001@gmail.com>
2026-01-06 15:56:08 +0530

    basic setup

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+ public static void main(String[] args) {
+    QuestionService services = new QuestionService();
+    services.displayQuestions();
+ }
+} 
---
+++ QuestionService.java
@@ -0,0 +1,10 @@
+public class QuestionService {
+
+    Question[] questions = new Question[5];
+    
+    public void displayQuestions (){
+        System.out.println("in display");
+    }
+}
+
+
